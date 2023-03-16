8dcca453374b3a0bc9999801c9d51afa358727aa
mahbubabbas <mahbubabbas@gmail.com>
2023-03-16 12:08:14 +0530

    folder renamed

--- myproj/myproj/settings.py
+++ myproj/myproj/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp'
+    'day3_common_template'
 ]
 
 MIDDLEWARE = [
--- myproj/myproj/urls.py
+++ myproj/myproj/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapp import views
+from day3_common_template import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
