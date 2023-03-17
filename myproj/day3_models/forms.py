2237881f337332837e6ec5503f53c1a9108289e2
mahbubabbas <mahbubabbas@gmail.com>
2023-03-17 15:37:38 +0530

    bootstrap

--- myproj/day3_models/forms.py
+++ myproj/day3_models/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class StudentForm(forms.Form):
-  fname = forms.CharField(max_length=50, label="First name")
-  lname = forms.CharField(max_length=50, label="Last name")
+  fname = forms.CharField(max_length=50, label="First name", widget=forms.TextInput(attrs={'class': 'form-control'}))
+  lname = forms.CharField(max_length=50, label="Last name", widget=forms.TextInput(attrs={'class': 'form-control'}))
